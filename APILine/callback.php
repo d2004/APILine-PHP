8d49f0e405e0b2825613380aa15de5f3ebfbc8fd
Davide Ramondetti <davideramondetti@yahoo.it>
2015-10-29 22:31:58 +0100

    ApiLine Callback v2
    ApiLine CallBack functions v2

--- APILine/callback.php
+++ APILine/callback.php
@@ -1,1 +1,10 @@
+<?php
+/*****************************************
+ * Copyright (c) 2015 D2004 on Github *
+ * https://github.com/d2004/APILine-PHP *
+ *             APILine PHP           *
+******************************************/
 
+class ApiLineCallBack {
+  
+}
